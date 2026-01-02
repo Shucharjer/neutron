7c8845a74e09c65fb6270ee7cabc3906e0be2a53
Shucharjer <Shucharjer@outlook.com>
2026-01-02 19:33:55 +0800

    [neutron] add concept `nothrow_conditional_movable`

--- neutron/include/neutron/concepts.hpp
+++ neutron/include/neutron/concepts.hpp
@@ -2,10 +2,10 @@
 // IWYU pragma: begin_exports
 #include "neutron/detail/concepts/allocator.hpp"
 #include "neutron/detail/concepts/map_like.hpp"
+#include "neutron/detail/concepts/nothrow_conditional_movable.hpp"
 #include "neutron/detail/concepts/one_of.hpp"
 #include "neutron/detail/concepts/pair.hpp"
 #include "neutron/detail/concepts/tag_invoke.hpp"
 #include "neutron/detail/concepts/trivially_relocatable.hpp"
 #include "neutron/detail/concepts/value.hpp"
-
 // IWYU pragma: end_exports
---
+++ neutron/include/neutron/detail/concepts/nothrow_conditional_movable.hpp
@@ -0,0 +1,12 @@
+// IWYU pragma: private, include "neutron/concepts.hpp"
+#pragma once
+#include <type_traits>
+
+namespace neutron {
+
+template <typename Ty>
+concept nothrow_conditional_movable =
+    std::is_nothrow_move_constructible_v<Ty> ||
+    std::is_nothrow_copy_constructible_v<Ty>;
+
+}
