45f4ebeb87cb20bb35ca4fb32d8a584e7231a28c
Shucharjer <Shucharjer@outlook.com>
2026-02-09 19:34:46 +0800

    [neutron] add test case for then with value

--- neutron/test/src/execution/then.cpp
+++ neutron/test/src/execution/then.cpp
@@ -1,6 +1,7 @@
 #define ATOM_EXECUTION
 #include <neutron/execution.hpp>
 #include <neutron/print.hpp>
+#include "require.hpp"
 
 using namespace neutron;
 using namespace neutron::execution;
@@ -14,6 +15,19 @@ int main() {
     {
         sender auto nsndr = just() | then([] { println("hello world"); });
         this_thread::sync_wait(nsndr);
+    }
+
+    {
+        sender auto vsndr = just(42) | then([](auto val) {
+                                println("first then: {}", val);
+                                return val << 1;
+                            }) |
+                            then([](auto val) {
+                                println("second then: {}", val);
+                                return val << 1;
+                            });
+        auto opt = this_thread::sync_wait(vsndr);
+        require(std::get<0>(opt.value()) == 42 << 2);
     }
 
     {
