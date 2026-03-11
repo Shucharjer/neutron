6ca37988dfb8323b48610f7a3b4a7f69f816802d
Shucharjer <Shucharjer@outlook.com>
2026-03-11 17:28:04 +0800

    [neutron] add concepts about tagged list

---
+++ neutron/include/neutron/detail/concepts/tagged.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include "neutron/detail/metafn/has.hpp"
+
+namespace neutron {
+
+template <typename T, typename Tag>
+concept type_list_with_tag = tagged_type_list_has_v<Tag, T>;
+
+template <typename T, typename Tag>
+concept value_list_with_tag = tagged_value_list_has_tag_v<Tag, T>;
+
+template <typename T, typename Tag>
+concept list_with_tag = tagged_list_has_tag_v<Tag, T>;
+
+}
