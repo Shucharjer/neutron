3093a5b60f52ea269028cd6c2546425eb38120d0
Shucharjer <2592284636@qq.com>
2026-04-15 23:43:27 +0800

    [concepts] update trivially_relocatable

--- include/neutron/detail/concepts/trivially_relocatable.hpp
+++ include/neutron/detail/concepts/trivially_relocatable.hpp
@@ -1,24 +1,28 @@
 // IWYU pragma: private, include <neutron/concepts.hpp>
 #pragma once
 
-namespace neutron {
-
 #ifndef __has_feature
     #define __has_feature(x) 0
 #endif
 
 #if __has_feature(__cpp_trivial_relocatability) &&                             \
-    __cpp_trivial_relocatability >= 202502L
+    __cpp_trivial_relocatability >= 202502L && false
+
+namespace neutron {
 
 template <typename Ty>
-using trivially_relocatable = std::is_trivially_relocatable_v<Ty>;
+using trivially_relocatable = std::is_trivially_relocatable_v<Ty>; // the prossible interface
 
-#elif __has_feature(reflection) && __cplusplus > 202302L
 }
 
-    #include <concepts>
-    #include <meta>
-    #include <type_traits>
+#else
+    #include "neutron/detail/macros.hpp"
+
+    #if ATOM_HAS_REFLECTION
+
+        #include <concepts>
+        #include <meta>
+        #include <type_traits>
 
 namespace neutron {
 
@@ -28,11 +32,11 @@ template <typename Ty>
 struct _is_trivially_relocatable;
 
 template <typename Ty>
-constexpr bool _has_any_virtual_base_class = [] {
-    constexpr auto ctx = std::meta::access_context::current();
-    template for (constexpr auto info :
-                  std::define_static_array(std::meta::bases_of(^^Ty, ctx))) {
-        if (std::meta::is_virtual(info)) {
+constexpr bool _has_any_virtual_base_class = [] consteval {
+    using namespace std::meta;
+    auto ctx = access_context::current();
+    for (auto info : std::define_static_array(bases_of(^^Ty, ctx))) {
+        if (is_virtual(info)) {
             return true;
         }
     }
@@ -40,12 +44,12 @@ constexpr bool _has_any_virtual_base_class = [] {
 }();
 
 template <typename Ty>
-constexpr bool _has_not_trivially_relocatable_base_class = [] {
-    constexpr auto ctx = std::meta::access_context::current();
+constexpr bool _has_not_trivially_relocatable_base_class = [] consteval {
+    using namespace std::meta;
+    constexpr auto ctx = access_context::current();
     template for (constexpr auto info :
-                  std::define_static_array(std::meta::bases_of(^^Ty, ctx))) {
-        if (!_is_trivially_relocatable<
-                typename[:std::meta::type_of(info):]>::value) {
+                  std::define_static_array(bases_of(^^Ty, ctx))) {
+        if (!_is_trivially_relocatable<typename[:type_of(info):]>::value) {
             return true;
         }
     }
@@ -53,17 +57,18 @@ constexpr bool _has_not_trivially_relocatable_base_class = [] {
 }();
 
 template <typename Ty>
-constexpr bool _has_not_trivially_relocatable_nonstatic_data_member = [] {
-    constexpr auto ctx = std::meta::access_context::current();
-    template for (constexpr auto info : std::define_static_array(
-                      std::meta::nonstatic_data_members_of(^^Ty, ctx))) {
-        if (!_is_trivially_relocatable<
-                typename[:std::meta::type_of(info):]>::value) {
-            return true;
+constexpr bool _has_not_trivially_relocatable_nonstatic_data_member =
+    [] consteval {
+        using namespace std::meta;
+        constexpr auto ctx = access_context::current();
+        template for (constexpr auto info : std::define_static_array(
+                          nonstatic_data_members_of(^^Ty, ctx))) {
+            if (!_is_trivially_relocatable<typename[:type_of(info):]>::value) {
+                return true;
+            }
         }
-    }
-    return false;
-}();
+        return false;
+    }();
 
 template <typename Ty>
 concept _eligible_for_relocation =
@@ -77,12 +82,11 @@ concept _has_trivially_relocatable_if_eligible = false;
 
 template <typename Ty>
 concept _union_without_user_defined_special_member_functions =
-    std::is_union_v<Ty> && [] {
-        constexpr auto ctx = std::meta::access_context::current();
-        template for (constexpr auto info : std::define_static_array(
-                          std::meta::members_of(^^Ty, ctx))) {
-            if (std::meta::is_special_member_function(info) &&
-                std::meta::is_user_provided(info)) {
+    std::is_union_v<Ty> && [] consteval {
+        using namespace std::meta;
+        auto ctx = access_context::current();
+        for (auto info : members_of(^^Ty, ctx)) {
+            if (is_special_member_function(info) && is_user_provided(info)) {
                 return false;
             }
         }
@@ -90,13 +94,12 @@ concept _union_without_user_defined_special_member_functions =
     }();
 
 template <typename Ty>
-concept _default_movable = [] {
-    constexpr auto ctx = std::meta::access_context::current();
-    template for (constexpr auto info :
-                  std::define_static_array(std::meta::members_of(^^Ty, ctx))) {
-        if (std::meta::is_move_constructor(info) ||
-            std::meta::is_move_assignment(info)) {
-            if (std::meta::is_user_provided(info)) {
+concept _default_movable = [] consteval {
+    using namespace std::meta;
+    auto ctx = access_context::current();
+    for (auto info : members_of(^^Ty, ctx)) {
+        if (is_move_constructor(info) || is_move_assignment(info)) {
+            if (is_user_provided(info)) {
                 return false;
             }
         }
@@ -122,12 +125,20 @@ template <typename Ty>
 concept trivially_relocatable =
     _trivially_relocatable::_is_trivially_relocatable<Ty>::value;
 
-#elif __has_builtin(__builtin_is_cpp_trivially_relocatable)
+} // namespace neutron
+
+    #elif __has_builtin(__builtin_is_cpp_trivially_relocatable)
+
+namespace neutron {
 
 template <typename Ty>
 concept trivially_relocatable = __builtin_is_cpp_trivially_relocatable(Ty);
 
-#elif !defined(__clang__) && __has_builtin(__is_trivially_relocatable)
+}
+
+    #elif !defined(__clang__) && __has_builtin(__is_trivially_relocatable)
+
+namespace neutron {
 
 /// @note: The implementation of `__is_trivially_relocatable` is since cxx03 and
 /// it does not implement the semantics of any current or future trivial
@@ -136,16 +147,19 @@ concept trivially_relocatable = __builtin_is_cpp_trivially_relocatable(Ty);
 template <typename Ty>
 concept trivially_relocatable = __is_trivially_relocatable(Ty);
 
-#else
-}
+} // namespace neutron
 
-    #include <type_traits>
+    #else
+
+        #include <type_traits>
 
 namespace neutron {
 
 template <typename Ty>
 concept trivially_relocatable = std::is_trivially_copyable_v<Ty>;
 
-#endif
-
 } // namespace neutron
+
+    #endif
+
+#endif
