de7fa6a49d8648a682a03edc051945581666699d
Shucharjer <Shucharjer@outlook.com>
2026-04-28 16:21:24 +0800

    [stop_token] add public header

--- include/neutron/stop_token.hpp
+++ include/neutron/stop_token.hpp
@@ -1,2 +1,14 @@
 #pragma once
 
+// IWYU pragma: begin_exports
+
+#include "neutron/detail/stop_token/inplace_stop_token.hpp"
+#include "neutron/detail/stop_token/inplace_stop_source.hpp"
+#include "neutron/detail/stop_token/inplace_stop_callback.hpp"
+#include "neutron/detail/stop_token/never_stop_token.hpp"
+#include "neutron/detail/stop_token/stop_token.hpp"
+#include "neutron/detail/stop_token/stop_source.hpp"
+#include "neutron/detail/stop_token/stoppable_token.hpp"
+#include "neutron/detail/stop_token/unstoppable_token.hpp"
+
+// IWYU pragma: end_exports
